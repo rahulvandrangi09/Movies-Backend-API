f08cf290cb9b333f083adcde7a918575d52bb9d6
rahulvandrangi09 <rahulvandrangi1k@gmail.com>
2026-02-22 10:54:41 +0530

    Added CORS configuration

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ import { connectDB, disconnectDB } from './config/db.js';
 import authRoutes from './routes/authRoutes.js';
 import dotenv from 'dotenv';
 import watchListRoutes from './routes/watchListRoutes.js'
+import cors from "cors";
+
 // import { PrismaClient } from "@prisma/client";
 const app = express();
 const port = 3000;
@@ -12,6 +14,10 @@ const port = 3000;
 dotenv.config();
 connectDB();
 
+app.use(cors({
+  origin: "https://api-movies-backend.onrender.com",
+  credentials: true
+}));
 
 //body passing middleware
 app.use(express.json());
